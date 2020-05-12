dda724215ac1c77d4d356ada9c113a7b363a2f2a
Attila Badi <attila.badi@gmail.com>
2020-05-12 08:46:07 +0000

    Yet another change

--- blog/settings.py
+++ blog/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['15683fa997b84c5ba5419c6ee222ad34.vfs.cloud9.eu-west-1.amazonaws.com']
+ALLOWED_HOSTS = ['15683fa997b84c5ba5419c6ee222ad34.vfs.cloud9.eu-west-1.amazonaws.com', 'blog-testapp-badiattila.herokuapp.com']
 
 
 # Application definition
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'blog.urls'
@@ -133,6 +134,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'posts/media')
